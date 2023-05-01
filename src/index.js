58bd49d3ef76a56a61995dad2fd092f7ba8786a2
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-05-01 22:46:03 +0200

    add fixes task-01

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let SEARCH_QUERY;
 const PER_PAGE = 40;
 let currentPage;
+let inputValue;
 
 const formEl = document.querySelector('#search-form');
-const inputEl = document.querySelector("[name='searchQuery']");
 const galleryEl = document.querySelector('.gallery');
 const guardEl = document.querySelector('.js-guard');
 
@@ -20,55 +19,66 @@ const options = {
 const observer = new IntersectionObserver(onPagination, options);
 const simpleLightbox = new SimpleLightbox('.gallery a');
 
-inputEl.addEventListener('input', onInput);
 formEl.addEventListener('submit', onFormSubmit);
 
-function onInput(e) {
-  SEARCH_QUERY = e.target.value.toLowerCase().trim();
-}
-
-function onFormSubmit(e) {
+async function onFormSubmit(e) {
   e.preventDefault();
-  if (!inputEl.value) {
+  inputValue = e.target.elements[0].value.toLowerCase().trim();
+  if (!inputValue) {
     return;
   }
   currentPage = 1;
   galleryEl.innerHTML = '';
   observer.unobserve(guardEl);
-  fetchData(PER_PAGE, currentPage, SEARCH_QUERY)
-    .then(({ data: { hits, totalHits } }) => {
-      if (!hits.length) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else if (hits.length > 0) {
-        galleryEl.innerHTML = createMarkup(hits);
-        simpleLightbox.refresh();
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-        if (currentPage !== Math.ceil(totalHits / PER_PAGE)) {
-          observer.observe(guardEl);
-        }
+
+  try {
+    const {
+      data: { hits, totalHits },
+    } = await fetchData(PER_PAGE, currentPage, inputValue);
+
+    if (!hits.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (hits.length > 0) {
+      galleryEl.innerHTML = createMarkup(hits);
+      simpleLightbox.refresh();
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+
+      if (currentPage !== Math.ceil(totalHits / PER_PAGE)) {
+        observer.observe(guardEl);
       }
-    })
-    .catch(err => console.log(err));
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function onPagination(entries, observer) {
-  entries.forEach(entry => {
+async function onPagination(entries, observer) {
+  if (!inputValue) {
+    observer.unobserve(guardEl);
+    Notify.info('Please enter a search term!');
+    return;
+  }
+  await entries.forEach(async entry => {
     if (entry.isIntersecting) {
       currentPage += 1;
-      fetchData(PER_PAGE, currentPage, SEARCH_QUERY)
-        .then(({ data: { hits, totalHits } }) => {
-          galleryEl.insertAdjacentHTML('beforeend', createMarkup(hits));
-          simpleLightbox.refresh();
-          if (currentPage === Math.ceil(totalHits / PER_PAGE)) {
-            observer.unobserve(guardEl);
-            Notify.info(
-              "We're sorry, but you've reached the end of search results."
-            );
-          }
-        })
-        .catch(err => console.log(err));
+
+      try {
+        const {
+          data: { hits, totalHits },
+        } = await fetchData(PER_PAGE, currentPage, inputValue);
+        galleryEl.insertAdjacentHTML('beforeend', createMarkup(hits));
+        simpleLightbox.refresh();
+        if (currentPage === Math.ceil(totalHits / PER_PAGE)) {
+          observer.unobserve(guardEl);
+          Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      } catch (err) {
+        console.log(err);
+      }
     }
   });
 }
